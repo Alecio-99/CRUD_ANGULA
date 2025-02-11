7eac32744b80845673fc17591f22174218caa1bb
Alecio <aleciosilveira97@gmail.com>
2025-02-11 17:44:54 -0300

    Ajustes

--- src/app/componente03/componente03.component.ts
+++ src/app/componente03/componente03.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Pessoas } from '../modelo/Pessoas';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-componente03',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './componente03.component.html',
   styleUrl: './componente03.component.scss'
 })
@@ -21,6 +22,8 @@ export class Componente03Component {
 
   vetor:Pessoas[] = [];
 
+  indice:number = -1;
+
   cadastrar(){
     //Cadastrar vetor
     this.vetor.push(this.formulario.value as Pessoas);
@@ -28,6 +31,37 @@ export class Componente03Component {
     //limpar inputs
      this.formulario.reset();
     //Vizualizar no console
-    console.table(this.vetor);
+    //console.table(this.vetor);
+  }
+  selecionar(indice:number){
+this.indice = indice;
+
+this.formulario.setValue({
+  nome : this.vetor[indice].nome,
+  idade : this.vetor[indice].idade,
+  cidade : this.vetor[indice].cidade
+});
+ 
+this.btnCadastrar = false;
+  }
+  alterar(){
+    this.vetor[this.indice] = this.formulario.value as Pessoas;
+
+    this.formulario.reset();
+
+    this.btnCadastrar = true;
+  }
+
+  remover(){
+    this.vetor.splice(this.indice, 1);
+
+    this.formulario.reset();
+
+    this.btnCadastrar = true;
+  }
+  cancelar(){
+    this.formulario.reset();
+
+    this.btnCadastrar = true;
   }
 }
